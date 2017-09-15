aeabd221e2dea626dc7239633adcf53eaae446bc
Ramesh Ramalingam <rrameshbtech@gmail.com>
2017-09-15 19:10:48 +0530

    Added header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    children: []
+    children:[]
+  },
+  {
+    path: 'users',
+    loadChildren: 'app/users/users.module#UsersModule'
   }
 ];
 
