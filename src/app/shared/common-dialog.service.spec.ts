65d3873b523952852e9bef8a2c4b5c63e873b304
Ramesh Ramalingam <rrameshbtech@gmail.com>
2017-11-13 16:08:21 +0530

    unit test cases for common dialog & exception services

--- src/app/shared/common-dialog.service.spec.ts
+++ src/app/shared/common-dialog.service.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed, inject, async } from '@angular/core/testing';
+import { TestBed, inject, async, fakeAsync, tick } from '@angular/core/testing';
 import { Component, Inject, NgModule } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 import { MaterialModule } from '../material.module';
 import { CommonDialogService } from './common-dialog.service';
+import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 
 @Component({
   template: `<section>Test Dialog</section>`
@@ -25,7 +27,7 @@ class TestModule {
 describe('CommonDialogService', () => {
   let overlayContainerElement: HTMLElement;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
       imports: [
         MaterialModule,
@@ -40,23 +42,46 @@ describe('CommonDialogService', () => {
             return { getContainerElement: () => overlayContainerElement };
           }
         }]
-    });
+    }).compileComponents();
   });
 
   it('should be created', inject([CommonDialogService], (dialogService: CommonDialogService) => {
     expect(dialogService).toBeTruthy();
   }));
 
-  it('should remove dialog on opening the dialog', async(() => {
-    inject([CommonDialogService], (dialogService: CommonDialogService) => {
+  it('should remove scroll on opening the dialog', async(
+    inject([CommonDialogService, MatDialog], (dialogService: CommonDialogService, dialog: MatDialog) => {
+
       expect(document.body.classList.contains('no-scroll')).toBe(false);
       let dialogRef = dialogService.openCustom(TestDialogComponent, {}, {});
 
       expect(document.body.classList.contains('no-scroll')).toBe(true);
-
-      dialogRef.close();
-      expect(document.body.classList.contains('no-scroll')).toBe(false);
     })
-  }));
+  ));
 
+  it('should open confirm dialog.', async(
+    inject([CommonDialogService, MatDialog], (dialogService: CommonDialogService, dialog: MatDialog) => {
+      
+      let mockOpen = spyOn(dialog, 'open');
+      dialogService.confirm('test title', 'test message');
+
+      expect(dialog.open).toHaveBeenCalled();
+      expect(mockOpen.calls.mostRecent().args[0].name).toBe('ConfirmDialogComponent');
+      expect(mockOpen.calls.mostRecent().args[1].data.title).toBe('test title');
+      expect(mockOpen.calls.mostRecent().args[1].data.message).toBe('test message');
+    })
+  ));
+  
+  it('should open custom dialog.', async(
+    inject([CommonDialogService, MatDialog], (dialogService: CommonDialogService, dialog: MatDialog) => {
+      
+      let mockOpen = spyOn(dialog, 'open');
+      dialogService.openCustom(TestDialogComponent, {test: 'succeed'}, {disableClose: true});
+
+      expect(dialog.open).toHaveBeenCalled();
+      expect(mockOpen.calls.mostRecent().args[0].name).toBe('TestDialogComponent');
+      expect(mockOpen.calls.mostRecent().args[1].data.test).toBe('succeed');
+      expect(mockOpen.calls.mostRecent().args[1].disableClose).toBe(true);
+    })
+  ));
 });
